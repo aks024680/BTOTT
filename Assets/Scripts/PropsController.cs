4edf0f0481724d8342bb66459a92f58351a4ab11
aks024680 <130197640+aks024680@users.noreply.github.com>
2024-06-18 21:39:07 +0800

    update 2024/06/18 打開背包跟關閉背包

---
+++ Assets/Scripts/PropsController.cs
@@ -0,0 +1,32 @@
+﻿
+using Fungus;
+using UnityEngine;
+
+namespace BTOTT
+{
+
+public class PropsController : MonoBehaviour
+{
+        [SerializeField]
+        GameObject SwitchPanel;
+
+        bool a = true;
+
+        private void Update()
+        {
+            if (a && Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.Escape))
+            {
+                SwitchPanel.SetActive(false);
+                a = false;
+            }
+            
+            else if (Input.GetKeyDown(KeyCode.I))
+            {
+                SwitchPanel.SetActive(true);
+                a = true;
+            }
+        }
+    }
+
+}
+

eaa736e8f50463081bbf6b367afc340d3bb4ba34
aks024680 <130197640+aks024680@users.noreply.github.com>
2024-06-22 11:14:00 +0800

    update 對話

---
+++ Assets/Scripts/NpcEntity.cs
@@ -0,0 +1,46 @@
+﻿using Fungus;
+using UnityEngine;
+
+public class NpcEntity : MonoBehaviour
+{
+    [Header("npc名字，需与Block名字一致")]
+    public string npcName;
+    private Flowchart flowchart;
+    private bool canSay;
+
+
+    void Start()
+    {
+        flowchart = GameObject.Find("Flowchart").GetComponent<Flowchart>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (canSay)
+            {
+                if (flowchart.HasBlock(npcName))
+                {
+                    flowchart.ExecuteBlock(npcName);
+                    print(npcName);
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canSay = true;
+            print(canSay);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canSay = false;
+            print(canSay);
+        }
+    }
+}
--- Assets/Scripts/TriggerBoxAnim.cs
+++ Assets/Scripts/TriggerBoxAnim.cs
@@ -5,7 +5,14 @@ namespace BTOTT
 {
     public class TriggerBoxAnim : MonoBehaviour
     {
+        public GameObject KeyE;
         public GameObject animationObject;
+
+        private void Start()
+        {
+            KeyE.SetActive(false);
+        }
+
         void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player"))
@@ -16,6 +23,17 @@ namespace BTOTT
                     ani.SetTrigger("Idle");
                     print(ani);
                 }
+                
+                KeyE.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                
+                KeyE.SetActive(false);
             }
         }
     }
